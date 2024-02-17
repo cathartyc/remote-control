0c1ba2e677f0f8bf1c1437c91000b7fc2b26995a
cathartyc <67878876+cathartyc@users.noreply.github.com>
2024-02-17 18:32:57 +0100

    use signal instead of handling SIGINT by hand

--- remote_control.py
+++ remote_control.py
@@ -1,17 +1,21 @@
 import os
 from time import sleep
+import signal
 from utils.controller import *
 
 if __name__ == "__main__": 
     os.system('stty -echo')
     controller = get_controller() 
     try:
+def signal_handler(sig, frame):
+    print("Closing...")
+    os.system('stty echo')
+    exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
         while True:
             try:
                 c_input = controller.read()
                 print(c_input)
             except OSError:
                 controller = get_controller()
-    except KeyboardInterrupt:
-        print("Closing")
-        os.system("stty echo")
